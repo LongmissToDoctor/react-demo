48cbea02c7e7a1e33f957314f0e3d0cd9fff6df7
wuzhiming <291324662@qq.com>
2022-11-15 23:28:34 +0800

    feat: update

--- src/components/sandbox/SideMenu.jsx
+++ src/components/sandbox/SideMenu.jsx
@@ -1,63 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Menu } from 'antd';
+import axios from 'axios';
 import {
-  PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
-  MailOutlined,
-  AppstoreOutlined,
-} from '@ant-design/icons';
-import {
-  // withRouter,
-  useHistory,
+  withRouter,
+  // useHistory,
 } from 'react-router-dom';
 import './sideMenu.css';
 
 const { Sider } = Layout;
 
-const menuList = [
-  {
-    key: '/home',
-    label: '首页',
-    icon: null,
-  },
-  {
-    key: '/user-manage',
-    label: '用户管理',
-    icon: null,
-    children: [
-      {
-        key: '/user-manage/list',
-        label: '用户列表',
-        icon: null,
-      },
-    ],
-  },
-  {
-    key: '/right-manage',
-    label: '权限管理',
-    icon: null,
-    children: [
-      {
-        key: '/right-manage/role/list',
-        label: '角色列表',
-        icon: null,
-      },
-      {
-        key: '/right-manage/right/list',
-        label: '权限列表',
-        icon: null,
-      },
-    ],
-  },
-];
-
 const SideMenu = React.memo(props => {
-  const history = useHistory();
+  const history = props.history;
+  const selectKeys = [props.location.pathname];
+  console.log(selectKeys);
+  const openKeys = ['/' + props.location.pathname.split('/')[1]];
+  const [collapsed, setCollapsed] = useState(false);
+  const [menuList, setMenuList] = useState([]);
+
   const onMenuClick = selector => {
     history.push(selector.key);
   };
-  const [collapsed, setCollapsed] = useState(false);
+
+  // todo 思考如何优雅处理数据
+  const handleMenuListData = dataList => {
+    for (let i = 0; i < dataList.length; i++) {
+      if (!dataList[i]?.children?.length) {
+        delete dataList[i].children;
+      }
+      if (dataList[i].children) {
+        dataList[i].children = dataList[i].children.filter(item => !!item?.pagepermisson);
+      }
+    }
+    return dataList;
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights?_embed=children').then(res => {
+      const newData = handleMenuListData(res?.data);
+      setMenuList(newData);
+    });
+  }, []);
   return (
     <Sider
       trigger={null}
@@ -65,17 +47,59 @@ const SideMenu = React.memo(props => {
       collapsed={collapsed}
       style={{ background: 'rgb(44, 44, 148)' }}
     >
-      <div className='logo'>全球新闻发布系统</div>
-      <Menu
-        theme='dark'
-        mode='inline'
-        defaultSelectedKeys={['1']}
-        items={menuList}
-        style={{ background: 'rgb(44, 44, 148)' }}
-        onClick={onMenuClick}
-      />
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <div className='logo'>全球新闻发布系统</div>
+        <div style={{ overflow: 'scroll' }}>
+          <Menu
+            theme='dark'
+            mode='inline'
+            selectedKeys={selectKeys}
+            defaultOpenKeys={openKeys}
+            items={menuList}
+            style={{ background: 'rgb(44, 44, 148)' }}
+            onClick={onMenuClick}
+          />
+        </div>
+      </div>
     </Sider>
   );
 });
 
-export default SideMenu;
+export default withRouter(SideMenu);
+
+// const menuList = [
+//   {
+//     key: '/home',
+//     label: '首页',
+//     icon: null,
+//   },
+//   {
+//     key: '/user-manage',
+//     label: '用户管理',
+//     icon: null,
+//     children: [
+//       {
+//         key: '/user-manage/list',
+//         label: '用户列表',
+//         icon: null,
+//       },
+//     ],
+//   },
+//   {
+//     key: '/right-manage',
+//     label: '权限管理',
+//     icon: null,
+//     children: [
+//       {
+//         key: '/right-manage/role/list',
+//         label: '角色列表',
+//         icon: null,
+//       },
+//       {
+//         key: '/right-manage/right/list',
+//         label: '权限列表',
+//         icon: null,
+//       },
+//     ],
+//   },
+// ];
--- src/pages/newsSandBox/right-manage/RightList.jsx
+++ src/pages/newsSandBox/right-manage/RightList.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, Tag, Button, Modal } from 'antd';
+import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import axios from 'axios';
+
+const { confirm } = Modal;
 
 const RightList = React.memo(() => {
-  return <div>RightList</div>;
+  const [dataSource, setDataSource] = useState([]);
+
+  const onDeleteHandler = item => {
+    if (item?.grade === 1) {
+      setDataSource(dataSource.filter(data => data.id !== item.id));
+      axios.delete(`http://localhost:5000/rights/${item.id}`);
+    } else {
+      console.log(item.id);
+    }
+  };
+
+  const showDeleteConfirm = item => {
+    confirm({
+      title: 'Are you sure delete this task?',
+      icon: <ExclamationCircleOutlined />,
+      content: 'Some descriptions',
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        onDeleteHandler(item);
+      },
+      onCancel() {
+        //
+      },
+    });
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      render: id => <b>{id}</b>,
+    },
+    {
+      title: '权限名称',
+      dataIndex: 'label',
+    },
+    {
+      title: '权限路径',
+      dataIndex: 'key',
+      render: key => <Tag color='orange'>{key}</Tag>,
+    },
+    {
+      title: '操作',
+      render: item => (
+        <div>
+          <Button
+            danger
+            shape='circle'
+            icon={<DeleteOutlined />}
+            onClick={() => showDeleteConfirm(item)}
+          ></Button>
+          <Button shape='circle' type='primary' icon={<EditOutlined />}></Button>
+        </div>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights?_embed=children').then(res => {
+      const list = res.data;
+      list.forEach(item => {
+        if (item.children && item.children.length === 0) {
+          item.children = '';
+        }
+      });
+      setDataSource(list);
+    });
+  }, []);
+  return <Table dataSource={dataSource} columns={columns} pagination={{ pageSize: 6 }} />;
 });
 
 export default RightList;
